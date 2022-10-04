478b14c8aec088d3102f9efa19ee65585897ea8e
MAYOWA <Balogun Mayowa>
2022-10-04 02:54:49 +0100

    recieving data from the post request

--- emailSender/emailApp/views.py
+++ emailSender/emailApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
 
+
+def sendEmail(request):
+    sender = request.POST['sender']
+    recipient = request.POST['recipient']
+    subject = request.POST['subject']
+    content = request.POST['content']
+
+    send_mail(subject ,content, sender , [recipient], fail_silently=False)
 # Create your views here.
